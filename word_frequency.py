b9775ea244f4081595b7ebcf69de564ea61e344a
Kathleen Rauh <krauh2010@gmail.com>
2015-12-02 14:03:37 -0500

    added Added if __name__ == '__main__': main

--- word_frequency.py
+++ word_frequency.py
@@ -5,7 +5,7 @@ def find_top_20(count_dict):
     # Sort results from most frequent to least.
     sorted_data = sorted(count_dict, key=lambda w: w[1])
     top_20 = (list(zip(sorted_data, count_dict[1])))
-    return top_20
+    print(top_20)
 
 
 def word_frequency(split_text):
@@ -20,18 +20,21 @@ def word_frequency(split_text):
 
 
 def main():
+    print
     #  Open .txt file and prepare contents for processing
     split_text = ''
+    print("split_text = ".format(split_text))
     line_lc = ''
-    with open("shakespeare.txt", newline='') as material:  # Import file in lines
+    with open("sample.txt", newline='') as material:  # Import file in lines
         for line in material:
             line_lc = re.sub(r'[^A-Za-z0-9\s]', "", (line.lower()))  # Strip lines
             split_text.append(line_lc.split())
             print("Another line!")
     word_frequency(split_text)
     find_top_20(count_dict)
-    print(top_20)
 
 
+if __name__ == '__main__':
+    main()
 #  make a list of the words, and then list(zip(~words~, range(20)))
 #    sorted(line_lc, key=lambda w[0])      #137, 230

2c348c268062c134083c414563a2aad2cf6c3c08
Kathleen Rauh <krauh2010@gmail.com>
2016-01-05 23:02:48 -0500

    Created docstrings, added read to main.

--- word_frequency.py
+++ word_frequency.py
@@ -2,15 +2,18 @@ import re
 
 
 def find_top_20(count_dict):
-    # Sort results from most frequent to least.
+    '''
+    Sort results from most frequent to least.
+    '''
     sorted_data = sorted(count_dict.items(), key=lambda w: w[1], reverse=True)
     return sorted_data[:20]
 
 
-def word_frequency(text):     # (split_text):
-    # Compare and count words
-    # for line in text:
-    # print(text)
+def word_frequency(text):
+    '''
+    Remove punctuation from text, make it lower case, split it into individual
+    words, compare and count words
+    '''
     line_lc = re.sub(r'[^A-Za-z0-9\s]', "", text).lower()  # Strip lines
     split_text = line_lc.split()    # Split the text on blank spaces
     count_dict = {}
@@ -23,9 +26,13 @@ def word_frequency(text):     # (split_text):
 
 
 def main():
-    # Open .txt file and call functions to process text
+    '''
+    Open .txt file and call functions to count individual words in the text, then
+    list top twenty most common words
+    '''
     with open("sample.txt", newline='') as f:
-        word_frequency(f)
+        text = f.read()
+        word_frequency(text)
         find_top_20()
         for word in find_top_20():
             print(word[0], word[1])
